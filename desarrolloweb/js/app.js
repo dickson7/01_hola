9be03672043e9dde80e6ec7daa112dba1194a7b1
dickson7 <garciadickson@hotmail.com>
2019-08-15 17:51:23 -0500

    Responsive design finalizado

--- desarrolloweb/js/app.js
+++ desarrolloweb/js/app.js
@@ -1,15 +1,29 @@
 console.log('Hola');
 console.log(document.querySelector('.menu'));
 
-
+//media querys en js
+const ipad = window.matchMedia('screen and (max-width: 767px)');
+// se definen la variables
 const menu = document.querySelector('.menu');
 console.log(menu);
 const burgerButton = document.querySelector('#burger-button');
 console.log(burgerButton);
 
-// se escucha el click y se llama la funcion hideShow
-burgerButton.addEventListener('click', hideShow)
 
+
+
+// se llama a una funcion para validar los cambios
+ipad.addListener(validacion)
+
+function validacion(evento){
+    if (evento.matches){
+        // se escucha el click y se llama la funcion hideShow
+        burgerButton.addEventListener('click', hideShow)
+    } else{
+        burgerButton.removeEventListener('click', hideShow)
+    }
+}
+validacion(ipad);
 function hideShow(){
     //se valida que el elemento menu contenga la clase
     if (menu.classList.contains('is-active')){
