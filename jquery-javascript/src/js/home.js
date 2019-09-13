92cc6cb0e945e97c25e168935ee6009a2270ee1a
dickson7 <garciadickson@hotmail.com>
2019-09-13 17:39:16 -0500

    fetch

--- jquery-javascript/src/js/home.js
+++ jquery-javascript/src/js/home.js
@@ -6,3 +6,12 @@ let cambia = "@LeonidasEsteban"
 function cambiarNombre(nuevoNombre) {
   cambia = nuevoNombre
 }
+
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then(function(response){
+    console.log(response)
+    return response.json()
+  })
+  .then(function (user){
+    console.log('user', user[0].name)
+  })
