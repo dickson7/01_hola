791eddecbe93034861f888f0a0e28f69c9591c0e
dickson7 <garciadickson@hotmail.com>
2019-09-16 14:05:03 -0500

    avances de clase 8 js

--- jquery-javascript/src/js/home.js
+++ jquery-javascript/src/js/home.js
@@ -7,6 +7,8 @@ function cambiarNombre(nuevoNombre) {
   cambia = nuevoNombre
 }
 
+
+// promesas (consumuir api con promesas)
 fetch('https://jsonplaceholder.typicode.com/users')
   .then(function(response){
     console.log(response)
@@ -15,3 +17,41 @@ fetch('https://jsonplaceholder.typicode.com/users')
   .then(function (user){
     console.log('user', user[0].name)
   })
+  .catch(function(){
+    console.log('Algo salio mal')
+  });
+
+  //asincronas (consumuir api con funtion async)
+  (async function load() {
+    //Funcion que realiza el get y resive la url y entrega el json
+    async function getData(url){
+      const response = await fetch(url);
+      const data = await response.json()
+      return data;
+    }
+    //se hace el llamado a la funcion y se almacena la data en cada variable
+    const actionList = await getData('https://yts.lt/api/v2/list_movies.json?genre=action')
+    const dramaList = await getData('https://yts.lt/api/v2/list_movies.json?genre=drama')
+    const animationList = await getData('https://yts.lt/api/v2/list_movies.json?genre=animation')
+    console.log('Acción', actionList, 'Terror', dramaList, 'Animación', animationList);
+
+    const $actionContainer = document.querySelector('#action');
+    const $dramaContainer = document.getElementById('drama');
+    const $animationContainer = document.getElementById('animation');
+
+    const $featuringContainer = document.getElementById('#featuring');
+    const $form = document.getElementById('#form');
+    const $home = document.getElementById('#home');
+
+    const $modal = document.querySelector('#modal');
+    const $overlay = document.getElementById('overlay');
+    const $hideModal = document.getElementById('hide-modal');
+
+    const $modalTitle = $modal.querySelector('h1');
+    const $modalDescription = $modal.querySelector('p');
+    const $modalImage = $modal.querySelector('img');
+
+
+
+  })()
+
